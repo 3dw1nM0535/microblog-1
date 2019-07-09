ac42ae03d51d324910fcff67d98997ef4b1ce230
3dw1nM0535 <workockmoses@gmail.com>
2019-07-10 00:04:09 +0300

    linting

--- app/models.py
+++ app/models.py
@@ -8,20 +8,20 @@ from time import time
 import jwt
 from app import app
 
-followers=db.Table('followers',
-    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-)
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
 
 class User(UserMixin, db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    username=db.Column(db.String(64), index=True, unique=True)
-    email=db.Column(db.String(120), index=True, unique=True)
-    password_hash=db.Column(db.String(128))
-    posts=db.relationship('Post', backref='author', lazy='dynamic')
-    about_me=db.Column(db.String(140))
-    last_seen=db.Column(db.DateTime, default=datetime.utcnow)
-    followed=db.relationship(
+    """User model"""
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
         'User',
         secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
@@ -34,13 +34,16 @@ class User(UserMixin, db.Model):
         return '<User {}>'.format(self.username)
 
     def set_password(self, password):
-        self.password_hash=generate_password_hash(password)
+        """Set user password to a hashed value"""
+        self.password_hash = generate_password_hash(password)
 
     def validate_password(self, password):
+        """Check user password"""
         return check_password_hash(self.password_hash, password)
 
     def avatar(self, size):
-        digest=md5(self.email.lower().encode('utf-8')).hexdigest()
+        """Generate user image avatar"""
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
     def follow(self, user):
@@ -48,42 +51,49 @@ class User(UserMixin, db.Model):
             self.followed.append(user)
 
     def unfollow(self, user):
+        """Unfollow user"""
         if self.is_following(user):
             self.followed.remove(user)
 
     def is_following(self, user):
+        """Check if user is following user"""
         return self.followed.filter(followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
+        """Query followed users posts"""
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id)
-        own=Post.query.filter_by(user_id=self.id)
+        own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
 
     def get_reset_password_token(self, expires_in=600):
+        """Encode jwt token with user id as payload"""
         return jwt.encode(
             {'reset_password': self.id, 'exp': time() + expires_in},
             app.config['SECRET'], algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
+        """Verify pasword reset token"""
         try:
-            id=jwt.decode(token, app.config['SECRET'], algorithm=['HS256'])['reset_password']
+            id = jwt.decode(token, app.config['SECRET'], algorithm=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
 
 
 class Post(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    body=db.Column(db.String(140))
-    timestamp=db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
+    """Post model"""
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
 @login.user_loader
 def load_user(id):
+    """Load user from the database using id"""
     return User.query.get(int(id))
